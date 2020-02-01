98b14208f34953172b0838c96d03e0def976c605
glaizaaa <48244070+glaizaaa@users.noreply.github.com>
2020-02-01 22:03:11 +0800

    Add files via upload

---
+++ SELECTION-SORT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelectionSort
+{
+ class SelectionSort
+{  
+    static void sort(int []arr) 
+    { 
+        int n = arr.Length; 
+        for (int i = 0; i < n - 1; i++) 
+
+        { 
+            int min_idx = i; 
+            for (int j = i + 1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+                    int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+
+    static void printArray(int []arr) 
+
+    { 
+        int n = arr.Length; 
+        for (int i=0; i<n; ++i) 
+            Console.Write(arr[i]+" "); 
+        Console.WriteLine(); 
+    } 
+
+    public static void Main() 
+
+    { 
+
+        int []arr = {30, 16, 5, 9, 2, 12, 4, 10};
+        Console.WriteLine("Array before sorting:");
+        printArray(arr);
+        sort(arr); 
+        Console.WriteLine("\nArray after sorting:"); 
+        printArray(arr); 
+    } 
+ } 
+}
+   
